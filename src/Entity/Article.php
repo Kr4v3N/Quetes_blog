42d1a461f8f50d327fa972a63602874496d23835
Kr4v3n <fafachen37@gmail.com>
2018-11-11 17:11:31 +0100

    update

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,9 +1,8 @@
-<?php
 
+<?php
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
@@ -15,47 +14,48 @@ class Article
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="string", length=100)
      */
     private $title;
-
     /**
      * @ORM\Column(type="text")
      */
     private $content;
-
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Category")
      * @ORM\JoinColumn(nullable=false)
      */
+    private $category;
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getTitle(): ?string
     {
         return $this->title;
     }
-
     public function setTitle(string $title): self
     {
         $this->title = $title;
-
         return $this;
     }
-
     public function getContent(): ?string
     {
         return $this->content;
     }
-
     public function setContent(string $content): self
     {
         $this->content = $content;
-
+        return $this;
+    }
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
         return $this;
     }
 }
